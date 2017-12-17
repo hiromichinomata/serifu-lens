1d0cbc9f75a2d3ee2367758166753060b779fd44
Hiromichi NOMATA <git@hiromichinomata.com>
2017-12-17 19:27:46 +0900

    Add merge image logic

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ import cv2
 message_box =cv2.imread('samples/message_box.png')
 hallway = cv2.imread('samples/hallway.jpg')
 
+m_h, m_w, _ = message_box.shape
+h_h, _, _ = hallway.shape
+hallway[h_h-m_h:h_h, 0:m_w] = message_box
+
 cv2.imwrite("output/result.png", hallway)
