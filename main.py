7699a6826c65de520c74e8cf292446f14a6aa713
Hiromichi NOMATA <git@hiromichinomata.com>
2017-12-17 17:57:42 +0900

    Add boilerplate output

--- main.py
+++ main.py
@@ -3,8 +3,5 @@ import cv2
 
 message_box =cv2.imread('samples/message_box.png')
 hallway = cv2.imread('samples/hallway.jpg')
-cv2.imshow('image', message_box)
-cv2.waitKey(0)
-cv2.imshow('image', hallway)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+cv2.imwrite("output/result.png", hallway)
